cdac29d0e09aae288bbe1ec1e325d1915837a033
Manu Kumar <143504163+Manukumar7644@users.noreply.github.com>
2026-04-23 23:15:25 +0530

    Add files via upload

---
+++ RestAPI-Ex-10/src/main/java/in/scalive/RestApiEx10Application.java
@@ -0,0 +1,13 @@
+package in.scalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiEx10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiEx10Application.class, args);
+	}
+
+}
