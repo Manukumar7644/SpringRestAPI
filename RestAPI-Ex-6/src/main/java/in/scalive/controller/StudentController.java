7a369a00626e6ff2b11f5a8a9966a90a9e9bdad9
Manu Kumar <143504163+Manukumar7644@users.noreply.github.com>
2026-04-24 23:46:38 +0530

    Add files via upload

---
+++ RestAPI-Ex-6/src/main/java/in/scalive/controller/StudentController.java
@@ -0,0 +1,39 @@
+package in.scalive.controller;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.scalive.model.Student;
+
+@RestController
+@RequestMapping(value="/api")
+public class StudentController {
+	private Map<Integer, Student>stdMap;
+	public StudentController() {
+		stdMap=new HashMap(Map.of(101,new Student(101,"Amit",45.6),102,new Student(102,"Deepak",87.3),103,new Student(103,"Gagan",67.2)));
+	}
+    @GetMapping("/students")
+    public Collection<Student> getAllStudents(){
+    	return stdMap.values();
+    }
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable Integer id) {
+    	Student s=stdMap.get(id);
+    	return s;
+    }
+    @PostMapping("/students")
+    public Student createStudent(@RequestBody Student s) {
+    	int id=100+stdMap.size()+1;
+    	s.setId(id);
+    	stdMap.put(id, s);
+    	return s;
+    }
+}
